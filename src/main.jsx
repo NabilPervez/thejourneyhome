ed3993b02cfc8f9fbe9f553bcd10af1a624a848d
Nabil Pervez <nabilpervez@Nabils-MacBook-Pro.local>
2025-12-15 08:43:09 -0600

    Migrate to Vite build system for proper React module support

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TheJourneyHome from './TheJourneyHome.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <TheJourneyHome />
+    </React.StrictMode>,
+)
